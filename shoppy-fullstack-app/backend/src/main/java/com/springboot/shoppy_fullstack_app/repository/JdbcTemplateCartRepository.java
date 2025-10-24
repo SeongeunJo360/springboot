2a11981ff296176a00ec8f7cb4a69ff91acddaae
Seongeunj <seongeunj360@gmail.com>
2025-10-24 16:52:53 +0900

    25-10-24

--- shoppy-fullstack-app/backend/src/main/java/com/springboot/shoppy_fullstack_app/repository/JdbcTemplateCartRepository.java
+++ shoppy-fullstack-app/backend/src/main/java/com/springboot/shoppy_fullstack_app/repository/JdbcTemplateCartRepository.java
@@ -1,11 +1,13 @@
 package com.springboot.shoppy_fullstack_app.repository;
 
 import com.springboot.shoppy_fullstack_app.dto.CartItem;
+import com.springboot.shoppy_fullstack_app.dto.CartListResponse;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 @Repository
 public class JdbcTemplateCartRepository implements CartRepository{
@@ -16,6 +18,30 @@ public class JdbcTemplateCartRepository implements CartRepository{
     }
 
     @Override
+    public List<CartListResponse> findList(CartItem cartItem) {
+        String sql = """
+               select * from view_cartlist where id = ?   
+                """;
+        Object[] params = { cartItem.getId() };
+        return jdbcTemplate.query(sql,
+                new BeanPropertyRowMapper<>(CartListResponse.class), params);
+    }
+
+    @Override
+    public int deleteItem(CartItem cartItem) {
+        String sql = """
+                delete from cart where cid = ?
+                """;
+        return jdbcTemplate.update(sql, cartItem.getCid());
+    }
+
+    @Override
+    public CartItem getCount(CartItem cartItem) {
+        String sql = "select ifnull(sum(qty), 0) as sumQty from cart where id = ?";
+        return jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(CartItem.class), cartItem.getId());
+    }
+
+    @Override
     public int updateQty(CartItem cartItem) {
         String sql = "";
         if(cartItem.getType().equals("+")) {
@@ -23,20 +49,28 @@ public class JdbcTemplateCartRepository implements CartRepository{
         } else {
             sql = " update cart set qty = qty - 1 where cid =? ";
         }
+        System.out.println("updateQty :: " + sql);
         return jdbcTemplate.update(sql, cartItem.getCid());
     }
 
     @Override
     public CartItem checkQty(CartItem cartItem) {
+        System.out.println("CartRepository :: " + cartItem.getPid() + cartItem.getSize() + cartItem.getId());
         String sql = """
-                SELECT cid, sum(pid=? AND size=? and id=?) AS checkQty
-                	FROM cart
-                	GROUP BY cid, id
-                	order by checkQty desc
-                	limit 1
+                SELECT
+                   ifnull(MAX(cid), 0) AS cid,
+                   COUNT(*) AS checkQty
+                 FROM cart
+                 WHERE pid = ? AND size = ? AND id = ?
                 """;
-        Object[] params = { cartItem.getPid(), cartItem.getSize(), cartItem.getId() };
-        return jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(CartItem.class), params);
+
+        Object[] params = {
+                cartItem.getPid(), cartItem.getSize(), cartItem.getId()
+        };
+        CartItem resultCartItem = jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(CartItem.class), params);
+
+        System.out.println("checkQty :: resultCartItem = " + resultCartItem);
+        return resultCartItem;
     }
 
     @Override
