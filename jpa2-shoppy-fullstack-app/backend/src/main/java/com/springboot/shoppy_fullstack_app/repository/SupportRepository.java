03bd15664da887fb562ad6e85fe61289987d687e
Seongeunj <seongeunj360@gmail.com>
2025-11-18 14:52:34 +0900

    25-11-18

--- jpa2-shoppy-fullstack-app/backend/src/main/java/com/springboot/shoppy_fullstack_app/repository/SupportRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.shoppy_fullstack_app.repository;
-
-import com.springboot.shoppy_fullstack_app.dto.SupportDto;
-import java.util.List;
-
-public interface SupportRepository {
-    List<SupportDto> findAll();
-    List<SupportDto> findAll(SupportDto support);
-}
